aa7c793e4964c3fa6ee3200ab7910d300d845529
kanegae-takumi <Zsdfg@outlook.jp>
2025-04-04 16:15:35 +0900

    a

---
+++ script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.sample-slider', {
+  loop: true, //ループ
+  autoplay: {
+    //自動再生
+    delay: 3000,
+  },
+  pagination: {
+    //ページネーション（ドット）
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    //ナビゲーション（矢印）
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
